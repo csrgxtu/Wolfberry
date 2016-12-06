03d9205398eeae27d1dce6eebde118d36756a0ec
csrgxtu <1246506786@qq.com>
2016-12-06 16:00:08 +0800

    only use map to split the signals in original_signal.json file

--- src/map.py
+++ src/map.py
@@ -1,18 +1,48 @@
 #!/usr/bin/env python
 
 import sys
+import json
+import bson
+
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+
+    # turn data into json dict
+    doc = json.loads(line)
+
+    for signal in doc['signals']:
+        print line
+        # assemble a doc here
+        if 'imei' in doc:
+            imei = doc['imei']
+        else:
+            imei = None
+
+        rec = {
+            'id': str(bson.ObjectId()),
+            'receiveTimestamp': doc['receiveTimestamp'],
+            'jid': doc['jid'],
+            'timestamp': doc['timestamp'],
+            'imei': imei,
+            'ssid': None,
+            'bssid': signal['bssid'],
+            'rssi': signal['rssi']
+        }
+
+        print '%s\t%s' % (rec['id'], json.dumps(rec))
+
+
+
+    # # split the line into words
+    # words = line.split()
+    # # increase counters
+    # for word in words:
+    #     # write the results to STDOUT (standard output);
+    #     # what we output here will be the input for the
+    #     # Reduce step, i.e. the input for reducer.py
+    #     #
+    #     # tab-delimited; the trivial word count is 1
+    #     print '%s\t%s' % (word, 1)
--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,3 @@
 hadoop jar /home/archer/bin/hadoop-2.7.2/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
   -file ./map.py -mapper ./map.py \
-  -file ./reduce.py -reducer ./reduce.py \
-  -input /usr/archer/data/5000-8.txt -output /usr/archer/tmp/output
+  -input /usr/archer/data/original_signal.json -output /usr/archer/tmp/output
