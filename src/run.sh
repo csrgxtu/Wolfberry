1992ad0037c528930043bde23f6666ed0ecb3d28
csrgxtu <1246506786@qq.com>
2016-12-06 15:08:39 +0800

    map 和 reduce代码还可以优化，即使用generator, 但是这里我只关注最基本的

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,4 @@
-hadoop jar /home/archer/bin/hadoop-2.7.2/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -file /home/archer/Documents/Python/Wolfberry/src/map.py -mapper /home/archer/Documents/Python/Wolfberry/src/map.py -file /home/archer/Documents/Python/Wolfberry/src/reduce.py -reducer /home/archer/Documents/Python/Wolfberry/src/reduce.py -input /usr/archer/data/5000-8.txt -output /usr/archer/tmp/output
+hadoop jar /home/archer/bin/hadoop-2.7.2/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
+  -file ./map.py -mapper ./map.py \
+  -file ./reduce.py -reducer ./reduce.py \
+  -input /usr/archer/data/5000-8.txt -output /usr/archer/tmp/output
